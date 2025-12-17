5c6e329d366ab3d5de8dcbf8200252dcfe0d46df
Dragos <dragoshuiu12@gmail.com>
2025-12-17 16:55:52 +0200

    Add code to show camera output

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,18 @@
 use opencv::prelude::*;
-fn main() {
-	println!("Hello, world!");
-
+use opencv::{videoio, highgui};
+fn main() -> opencv::Result<()> {
+	let mut cam = videoio::VideoCapture::new(0, videoio::CAP_V4L2)?;
+	if !videoio::VideoCapture::is_opened(&cam)? {
+		panic!("The cam did not open");
+	}
+	loop {
+		let mut frame = Mat::default();
+		cam.read(&mut frame)?;
+		highgui::imshow("cam", &frame)?;
+		if highgui::wait_key(10)? == 'q' as i32 {
+			break;
+		}
+	}
+	highgui::destroy_all_windows()?;
+	Ok(())
 }
