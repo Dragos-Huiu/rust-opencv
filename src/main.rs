e03532c2919ee7f9b07682ac52bff284896bb27a
Dragos <dragoshuiu12@gmail.com>
2025-12-18 22:43:14 +0200

    Bug fixes and more readable code

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,44 @@
 use opencv::prelude::*;
-use opencv::{videoio, highgui, imgcodecs, core};
+use opencv::{videoio, highgui, imgcodecs};
 use std::fs;
-use crate::imgcodecs::ImwriteFlags::IMWRITE_JPEG_QUALITY;
+use std::path::Path;
+
+const SCREENSHOT_CODE: i32 = 115; // 's'
+const QUIT_CODE: i32 = 113; // 'q'
 
 fn main() -> opencv::Result<()> {
 	let mut cam = videoio::VideoCapture::new(0, videoio::CAP_V4L2)?;
 	if !videoio::VideoCapture::is_opened(&cam)? {
 		panic!("The cam did not open");
 	}
-	let mut i = 0;
-	fs::create_dir("/screenshots");
-	loop {
+	if !Path::new("./screenshots").exists() {
+		fs::create_dir("./screenshots").expect("Directory cannot be created");
+	}
+	
+	let mut screenshot_index = 0;
+	while cam.is_opened()? {
 		let mut frame = Mat::default();
 		cam.read(&mut frame)?;
 		highgui::imshow("cam", &frame)?;
 
-		if highgui::wait_key(10)? == 's' as i32 {
-			let params = opencv::core::Vector::default();
-			imgcodecs::imwrite(&format!("img{i}.jpeg"), &frame, &params);
-			i += 1
-		}
-		if highgui::wait_key(10)? == 'q' as i32 {
-			break;
+
+		let key = highgui::wait_key(5)?;
+		match key {
+			SCREENSHOT_CODE => {
+				let params = opencv::core::Vector::default();
+				imgcodecs::imwrite(&format!("./screenshots/img{screenshot_index}.jpeg"),
+							   &frame, &params)?;
+				screenshot_index += 1
+			},
+			QUIT_CODE => {
+				cam.release()?;
+			},
+			_ => {
+
+			},
 		}
 	}
+
 	highgui::destroy_all_windows()?;
 	Ok(())
 }
